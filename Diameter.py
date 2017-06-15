c24c293f12a054f45c2ef25650ad11ccbe2347c5
Eli Zupke <eazupke@cs.hmc.edu>
2017-06-15 10:59:37 -0700

    Finished Implementation of ComputePathSymmetricSetDifferenceTable() Added example2

--- Diameter.py
+++ Diameter.py
@@ -1,34 +1,60 @@
 import DP
 
-def FindValidPaths(Key,PreviousValues,Tree):
-    """A recursive algorithm to find all of the valid paths through a binary tree."""
+def FindValidPaths(Key, PreviousValues, Tree):
+    """A recursive algorithm to find all of the valid paths through a binary tree, as well as a dict containing the
+    nodes in each path."""
 
     #The tree is represented as a dict, with values having the structure (previousNode,thisNode,childNode1,childNode2)
-    #With the child nodes values being the keys to those "nodeSet"s, as I've called them here
+    #With the child nodes values being the keys to those "NodeSet"s, as I've called them here
 
-    nodeSet = Tree[Key]
-    node = nodeSet[1]
-    paths = [(node, node)]
-    for val in PreviousValues:
-        paths += [(val,node)]
-    if not nodeSet[2] == None: #Then this node is not a leaf node, so we need to add this node's children
-        paths += FindValidPaths(nodeSet[2], PreviousValues + [node], Tree)
-        paths += FindValidPaths(nodeSet[3], PreviousValues + [node], Tree)
-    return paths
+    NodeSet = Tree[Key]
+    Node = NodeSet[1]
+    NextValues = PreviousValues + [Node]  # This list contains every node we visited to get here from the root
+    Paths = []
+    PathNodes = {}
+    for i in range(0, len(NextValues)):
+        SourceNode = NextValues[i]
+        NewPath = (SourceNode, Node)  # This becomes every path that ends in this value, including A->A
+        Paths += [NewPath]
+        PathNodes[NewPath] = NextValues[i:]  # We need to add every node after and including the SourceNode
+
+    if not NodeSet[2] == None:  # Then this Node is not a leaf Node, so we need to add this Node's children
+        Child1Paths, Child1PathNodes = FindValidPaths(NodeSet[2], NextValues, Tree)
+        Child2Paths, Child2PathNodes = FindValidPaths(NodeSet[3], NextValues, Tree)
+        Paths += Child1Paths + Child2Paths
+        Child1PathNodes.update(Child2PathNodes)
+        PathNodes.update(Child1PathNodes)
+                                # Otherwise, we have reached the end of the tree (the base case)
+    return Paths, PathNodes
 
 
 
 def ComputePathSymmetricSetDifferenceTable(sTree):
     """Computes the table containing the number of nodes in the symmetric set difference between any two paths on the
-     species tree."""
+     species tree sTree. This is used in assigning a score to two paths' losses."""
 
-    PathList = FindValidPaths('hTop',[],sTree)
+    PathList, PathNodes = FindValidPaths('hTop', [], sTree) # The handle of sTree is 'hTop', as given by newickFormatReader.py
 
-
-    # TODO: everything
     # Note: If you wish to modify the code to assign different costs for each gene node, modifying this function
     # to provide the actual lists of nodes in the SSD might be a good place to start
-    return {}, PathList
+
+    SSD = {} #This is the Symmetric Set Difference table we will be returning
+
+    # The algorithm described in Jordan's writeup to find the intersection did not make sense to me, and I don't know
+    # whether it applies to the SSD
+
+    NonTrivialPathList = list(PathList) # Strip the trivial paths because they will not have loss events
+    for Path in NonTrivialPathList:
+        if Path[0] == Path[1]:
+            NonTrivialPathList.remove(Path)
+
+    for PathA in NonTrivialPathList:
+        ANodes = frozenset(PathNodes[PathA])
+        for PathB in NonTrivialPathList:
+            BNodes = frozenset(PathNodes[PathB])
+            SSD[(PathA, PathB)] = len(ANodes.symmetric_difference(BNodes))
+
+    return SSD, PathList, NonTrivialPathList
 
 
 def ComputeTrivialExitEventTable(u, ExitEvent):
@@ -58,6 +84,17 @@ def ComputeEnterMappingTable(u):
     # TODO: Add function body
 
 
+def PrintPathTableNicely(PathList,PathTable):
+    line = "\t"
+    for Px in PathList:
+        line += str(Px[0]) + "->" + str(Px[1]) + "\t"
+    print line
+    for Py in PathList:
+        line = str(Py[0]) + "->" + str(Py[1]) + ":\t"
+        for Px in PathList:
+            line += str(PathTable[(Px,Py)]) +"\t"
+        print line
+
 def CalculateDiameter(filename, D, T, L):
     """This function computes the diameter of space of MPRs in a DTL reconciliation problem,
     as measured by the symmetric set distance between the two."""
@@ -65,9 +102,12 @@ def CalculateDiameter(filename, D, T, L):
     # TODO: Add a way to run the CalculateDiameter function on a previously-found reconciliation.
 
     SpeciesTree, RecTree = DP.reconcile(filename, D, T, L)
-    PathSymmetricSetDifference = {} # A dict containing the SSD for each non-trivial pair of species nodes.
+    print "Reconciliation Complete"
+    PathSymmetricSetDifference = {} # A dict containing the SSD count for each pair of species node paths.
 
-    PathList = []   # A list of all of the valid paths between two species nodes
+    PathList = []   # A list of all of the valid paths between two species nodes (format is (src,dest))
+
+    NonTrivialPathList = []  # A subset of the elements in PathList where src != dest
 
     # The key format for the next three dicts are as follows: (u, x, y), where u is a node on the gene tree, and
     # x and y are either event nodes (represented as E1 and E2) or mapping nodes (represented as uA and uB).
@@ -81,6 +121,6 @@ def CalculateDiameter(filename, D, T, L):
     EnterMapping = {}   # A dict containing the largest number of event nodes that each pair of reconciliation subtrees
                         # rooted at uA and uB can have in common.
 
-    PathSymmetricSetDifference, PathList = ComputePathSymmetricSetDifferenceTable(SpeciesTree)
+    PathSymmetricSetDifference, PathList, NonTrivialPathList = ComputePathSymmetricSetDifferenceTable(SpeciesTree)
     print PathList
 
